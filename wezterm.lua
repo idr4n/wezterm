a95aa4bc3bf1921f52cca632c366cd11bdc45dc6
Ivan Duran <idurandiaz@gmail.com>
2024-08-15 12:38:48 +0300

    Latest updates and formatting

--- wezterm.lua
+++ wezterm.lua
@@ -18,12 +18,22 @@ schemeGithubLight.cursor_bg = "#9775FA"
 schemeGithubLight.cursor_border = "#9775FA"
 -- customize Monokai-pro
 local schemeMonokaiPro = wezterm.get_builtin_color_schemes()["Monokai (base16)"]
-schemeMonokaiPro.background = "#2A2A2A"
+schemeMonokaiPro.background = "#2D2A2E"
 -- schemeMonokaiPro.cursor_fg = "#2D2A2E"
 -- schemeMonokaiPro.cursor_bg = "#FFC22D"
 -- customize OneDark
 local schemeOneDark = wezterm.get_builtin_color_schemes()["OneDark (base16)"]
 -- schemeOneDark.background = "#1E222A"
+schemeOneDark.ansi = {
+  "#1E2127",
+  "#E06C75",
+  "#98C379",
+  "#E5C07B",
+  "#61AFEF",
+  "#C678DD",
+  "#56B6C2",
+  "#ABB2BF",
+}
 -- schemeOneDark.cursor_fg = "#1F2329"
 -- schemeOneDark.cursor_fg = "#E1E1E1"
 schemeOneDark.cursor_bg = "#74ADE8"
@@ -51,46 +61,40 @@ catppuccinFrappe.tab_bar.inactive_tab.fg_color = catppuccinFrappe.brights[8]
 catppuccinFrappe.tab_bar.active_tab.bg_color = catppuccinFrappe.background
 catppuccinFrappe.tab_bar.active_tab.fg_color = catppuccinFrappe.ansi[6]
 catppuccinFrappe.tab_bar.active_tab.intensity = "Bold"
+local schemeGithubDark = wezterm.get_builtin_color_schemes()["GitHub Dark"]
+schemeGithubDark.background = "#0D1117"
+schemeGithubDark.cursor_bg = "#58a6ff"
+-- schemeGithubDark.cursor_bg = "#ed6a43"
+schemeGithubDark.split = "#282828"
+local schemeTokyoNightMoon = wezterm.get_builtin_color_schemes()["tokyonight_moon"]
+schemeTokyoNightMoon.split = "#3A3A3A"
+schemeTokyoNightMoon.cursor_bg = "#BD93F9"
+schemeTokyoNightMoon.cursor_border = "#BD93F9"
+local schemeDracula = wezterm.get_builtin_color_schemes()["Dracula (Official)"]
+-- schemeDracula.background = "#22212C"
+schemeDracula.cursor_bg = "#BD93F9"
+schemeDracula.cursor_border = "#BD93F9"
+schemeDracula.split = "#394362"
 
 local function scheme_for_appearance(appearance)
   if appearance:find("Dark") then
-    -- return "zenbones_dark custom"
-    -- return "Wind"
-    -- return "OneDark Custom"
-    -- return "Everforest Dark (Gogh)"
-    -- return "OneDark (base16)"
-    -- return "tokyonight_night"
-    -- return "tokyonight_moon"
-    -- return "Kanagawa (Gogh)"
-    -- return "duskfox"
-    -- return "nordfox"
-    -- return "rose-pine-moon"
-    -- return "Catppuccin Macchiato"
-    -- return "Catppuccin Mocha"
-    -- return "Oxocarbon Dark"
-    return "Catppuccin Frappe"
-    -- return "Monokai (base16)"
-    -- return "Gruvbox Material (Gogh)"
-    -- return "Solarized (dark) (terminal.sexy)"
-  else
-    -- return "zenbones_light custom"
-    -- return "dawnfox"
-    -- return "rose-pine-dawn"
-    -- return "nordfox"
-    -- return "tokyonight_moon"
-    -- return "Kanagawa (Gogh)"
-    -- return "tokyonight_night"
-    return "Github Light"
-    -- return "seoulbones_dark"
-    -- return "Oxocarbon Dark"
-    -- return "rose-pine-moon"
-    -- return "OneDark Custom"
-    -- return "Everforest Dark (Gogh)"
-    -- return "OneDark (base16)"
     -- return "Catppuccin Frappe"
-    -- return "Monokai (base16)"
     -- return "Catppuccin Macchiato"
     -- return "Catppuccin Mocha"
+    -- return "Dracula Custom"
+    -- return "GitHub Dark Custom"
+    -- return "Monokai (base16)"
+    return "OneDark Custom"
+    -- return "tokyonight_moon_custom"
+  else
+    -- return "Catppuccin Frappe"
+    -- return "Catppuccin Macchiato"
+    -- return "Catppuccin Mocha"
+    -- return "Github Light"
+    -- return "Monokai (base16)"
+    -- return "OneDark Custom"
+    -- return "tokyonight_moon_custom"
+    return "Dracula Custom"
   end
 end
 
@@ -101,9 +105,12 @@ local function autoThemeOptions()
       win_opacity = 1,
       win_blur = 5,
       -- font = "Iosevka Custom Light Extended",
-      font = "Monolisa",
+      -- font = "MonoLisa",
+      -- font = "MonoLisa",
+      font = "MonoLisa Nerd Font",
       -- font = "Operator Mono Lig",
-      font_weight = "Light",
+      -- font_weight = "Light",
+      font_weight = "Regular",
       border_color = "#C093B7",
       -- berder_width = "0.38cell",
       -- berder_height = "0.15cell",
@@ -115,10 +122,11 @@ local function autoThemeOptions()
       win_opacity = 1,
       win_blur = 5,
       -- font = "Iosevka Custom Extended",
-      font = "Monolisa",
+      -- font = "MonoLisa",
+      font = "MonoLisa Nerd Font",
       -- font = "Operator Mono Lig",
-      -- font_weight = "Regular",
-      font_weight = "Light",
+      font_weight = "Regular",
+      -- font_weight = "Light",
       border_color = "#C093B7",
       border_width = 0,
     }
@@ -130,6 +138,15 @@ local function font_with_fallback(name, params)
   local names = { name, "Noto Color Emoji", "JetBrains Mono" }
   return wezterm.font_with_fallback(names, params)
 end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local pane = tab.active_pane
+  local cwd = pane.current_working_dir
+  return {
+    -- { Text = " " .. " hello " .. " " }, -- this works
+    { Text = " " .. cwd .. " " }, -- this doesn't work
+  }
+end)
 
 return {
   -- debug_key_events = true,
@@ -161,8 +178,8 @@ return {
   -- window_close_confirmation = "NeverPrompt",
   -- window_background_opacity = autoThemeOptions().win_opacity,
   -- macos_window_background_blur = autoThemeOptions().win_blur,
-  -- window_background_opacity = 0.97,
-  macos_window_background_blur = 10,
+  -- window_background_opacity = 0.90,
+  -- macos_window_background_blur = 30,
   inactive_pane_hsb = {}, -- don't dim inactive panes
   send_composed_key_when_left_alt_is_pressed = false,
   send_composed_key_when_right_alt_is_pressed = false,
@@ -193,62 +210,53 @@ return {
   cursor_thickness = 3,
   -- default_cursor_style = "BlinkingBar",
   -- this is for FiraCode
-  -- font_size = 15,
+  -- font_size = 16,
   -- line_height = 1.25,
+  -- font_size = 16,
+  -- line_height = 1.28,
+  -- cell_width = 0.95,
   -- this is for source code pro
-  font_size = 16,
-  line_height = 1.09,
+  -- font_size = 16,
+  -- line_height = 1.09,
   -- this is for Iosevka Custom Extended
   -- font_size = 14,
   -- line_height = 1.25,
   -- this is for Liga SFMono
   -- line_height = 1.21,
   -- this is for Monolisa {{{
-  -- font_size = 13,
-  -- line_height = 1.15,
-  -- font_size = 14,
-  -- line_height = 1.2,
+  -- font_size = 15,
   -- line_height = 1.12,
-  -- font_size = 12,
-  -- line_height = 1.25,
+  font_size = 17,
+  line_height = 1.23,
   -- }}}
   -- -- this is for Operator Mono
   -- font_size = 15,
   -- line_height = 1.15,
   term = "wezterm",
-  -- font = wezterm.font("Monolisa Custom"),
-  -- font = wezterm.font("Monolisa Custom", { weight = "Light" }),
-  -- font = wezterm.font_with_fallback({ { family = "Monolisa Custom", weight = "Light" }, "nonicons" }),
-  -- font = wezterm.font_with_fallback({
-  --     { family = "Monolisa", weight = "Light" },
-  --     -- { family = "MonolisaCustom Nerd Font", weight = "Light" },
-  --     -- { family = "MonolisaLight Nerd Font" },
-  --     -- { family = "Liga SFMono Nerd Font", weight = "Light" },
-  --     -- { family = "MonolisaCustom Nerd Font", weight = "Regular" },
-  --     -- { family = "Symbols Nerd Font Mono", scale = 0.8 },
-  -- }),
+  -- font = wezterm.font(autoThemeOptions().font, { weight = autoThemeOptions().font_weight }),
   -- For Fira Code it is needed to set a rule so Wezterm does not use JetBrains Mono italics
   -- font = font_with_fallback("Fira Code", { weight = autoThemeOptions().font_weight }),
-  -- font = wezterm.font(autoThemeOptions().font, { weight = autoThemeOptions().font_weight }),
   -- font = font_with_fallback("Iosevka SS04", { weight = "Regular" }),
-  -- font = font_with_fallback("FiraCode Nerd Font", { weight = "Regular" }),
-  font = font_with_fallback("SauceCodePro Nerd Font", { weight = "Regular" }),
-  font_rules = {
-    {
-      italic = true,
-      -- font = font_with_fallback("Fira Code", { weight = autoThemeOptions().font_weight }),
-      -- font = font_with_fallback("Iosevka SS04", { weight = "Regular" }),
-      font = font_with_fallback("SauceCodePro Nerd Font", { weight = "Regular" }),
-    },
-  },
+  -- font = font_with_fallback("FiraCode Nerd Font", { weight = autoThemeOptions().font_weight }),
+  font = font_with_fallback("ZedPlexMono Nerd Font Extended", { weight = autoThemeOptions().font_weight }),
+  -- font = font_with_fallback("SauceCodePro Nerd Font", { weight = "Regular" }),
+  -- font_rules = {
+  --   {
+  --     italic = true,
+  --     font = font_with_fallback("FiraCode Nerd Font", { weight = autoThemeOptions().font_weight }),
+  --     -- font = font_with_fallback("ZedPlexMono Nerd Font Extended", { weight = autoThemeOptions().font_weight }),
+  --     -- font = font_with_fallback("Iosevka SS04", { weight = "Regular" }),
+  --     -- font = font_with_fallback("SauceCodePro Nerd Font", { weight = "Regular" }),
+  --   },
+  -- },
   -- For Fira Code and Monolisa:
   -- harfbuzz_features = { "zero", "ss04", "cv16", "cv14", "calt=0", "clig=0", "liga=0" },
-  -- harfbuzz_features = { "zero", "ss04", "cv16", "cv14" },
+  harfbuzz_features = { "zero", "ss04", "cv16", "cv14" },
   -- For Monolisa >= ==:
   -- harfbuzz_features = { "liga=0" },
 
   -- key bindings
-  keys = require("wezterm_keys"),
+  keys = require("wezterm_keys").keys,
 
   -- color schemes
   color_schemes = {
@@ -262,6 +270,9 @@ return {
     ["Gruvbox Material (Gogh)"] = schemeGruvbox,
     ["OneDark Custom"] = schemeOneDark,
     ["Wind"] = schemeWind,
+    ["GitHub Dark Custom"] = schemeGithubDark,
+    ["tokyonight_moon_custom"] = schemeTokyoNightMoon,
+    ["Dracula Custom"] = schemeDracula,
   },
   color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 }
